0bec5eba678f0d326a4dd442d5821f0dc28583c5
Anoshsantiago <anoshsantiago01@gmail.com>
2025-02-28 06:42:46 +0100

    second commit.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public  static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("I am a student");
+        System.out.println("My name is Anosh");
+        System.out.println("I am a student. ");
     }
 }
